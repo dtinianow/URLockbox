a8e4e0094bbe5225fdf5f8eaaa818cc3113467fb
David Tinianow <david.tinianow@gmail.com>
2016-10-31 12:30:48 -0600

    Link submission box says please enter a url if http is not given

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,6 +2,7 @@ require 'uri'
 
 class Link < ActiveRecord::Base
   belongs_to :user
-  validates :url, presence: true, format: URI::regexp(%w(http https))
+  validates :url, presence: true,
+                  format: URI::regexp(%w(http https))
   validates :title, presence: true
 end
