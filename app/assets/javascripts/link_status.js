79e6ead653756d4b8bc5f673bb57f38361f0fe45
David Tinianow <david.tinianow@gmail.com>
2016-10-31 16:32:21 -0600

    Cannot figure why status will not change in database

--- app/assets/javascripts/link_status.js
+++ app/assets/javascripts/link_status.js
@@ -8,19 +8,26 @@ function onClickChangeReadStatus() {
     var link_id = $link.data('link-id');
     var user_id = $link.data('user-id');
     var $linkStatus = $link.find('.link-read-status')
-    var status = $linkStatus.text();
+    var $linkButton = $link.find('.read-status')
+    var status = $linkStatus.text().toString();
     var newStatus = !status;
-    changeReadStatus(newStatus, link_id, user_id, $linkStatus);
+    changeReadStatus(newStatus, link_id, user_id, $linkStatus, $linkButton);
   })
 };
 
-function changeReadStatus(newStatus, link_id, user_id, $linkStatus) {
+function changeReadStatus(newStatus, link_id, user_id, $linkStatus, $linkButton) {
   $.ajax({
     url: "/api/v1/users/" + user_id + "/links/" + link_id,
     type: 'put',
     data: { link: { read: newStatus } }
   }).fail(handleError)
   $linkStatus.text(newStatus);
-}
+  if ($linkButton.text() == 'Mark as Read') {
+    $linkButton.text('Mark as Unread');
+  } else {
+      $linkButton.text('Mark as Read');
+    };
+    // console.log($linkButton.text())
+  };
 
-function handleError(error) { console.log(error) };
+  function handleError(error) { console.log(error) };
