47e407491778d4f22af088f8a78df459e105109d
David Tinianow <david.tinianow@gmail.com>
2016-10-31 14:17:10 -0600

    Begin building jQuery function to change read status

--- app/assets/javascripts/link_status.js
+++ app/assets/javascripts/link_status.js
@@ -1,44 +1,30 @@
 $(document).ready(function() {
-  checkListStatus();
+  onClickChangeReadStatus();
 });
 
+function onClickChangeReadStatus() {
+  $('#links-table').on('click', '.read-link', function() {
+    var $link = $(this).closest('tr');
+    var $linkStatus = $link.find('.link-read-status')
+    var status = $linkStatus.text();
+    var newStatus = !status;
+    console.log(newStatus)
+    // changeReadStatus();
+  })
+};
 
-// function getIdeas() {
+// function changeReadStatus(newStatus) {
 //   $.ajax({
-//     type: 'get',
-//     url: '/api/v1/links'
-//   }).then(collectLinks)
-//   .then(renderLink)
-//   .fail(handleError)
-// };
 //
-// function collectLinks( linksData ) {
-//   return linksData.map(createLinkHTML);
-// };
-//
-// function createLinkHTML( link ) {
-//   return $(
-//     "<tr class=link data-id='"
-//     + idea.id
-//     + "'><td class=link-title contenteditable='true'>"
-//     + link.title
-//     + "</td><td class=link-body contenteditable='true'>"
-//     + truncate(link.body)
-//     + "</td><td class=link-quality>"
-//     + link.quality
-//     + "<td><button type='button' class='btn btn-success upvote-link'>Upvote</button></td>"
-//     + "<td><button type='button' class='btn btn-primary downvote-link'>Downvote</button></td>"
-//     + "</td><td><button class='btn btn-danger delete-link'>Delete</button></td>"
-//     + "</tr>"
-//   )
-// };
-// function renderLink( linkData ) {
-//   $('#links-table').prepend(linkData);
-// };
+//   })
+// }
 
+// function editIdeaQuality(newQuality, $idea, $quality){
+//   $.ajax({
+//     url: '/api/v1/ideas/' + $idea.data('id'),
+//     type: 'put',
+//     data: { idea: { quality: newQuality } }
+//   }).fail(handleError)
+//   $quality.text(newQuality);
+// }
 //
-// function truncate( body ) {
-//   return body.split(' ').slice(0, 100).join(" ")
-// };
-
-// function handleError( error ) { console.log(error) };
