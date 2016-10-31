bbb18880a3ac200c24cd96934cff05976b8f3635
David Tinianow <david.tinianow@gmail.com>
2016-10-31 14:42:06 -0600

    Write initial test for user editing a link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def check_link_status(link_status)
     if link_status
-      '<td><button class="read-link">Mark as Unread</button></td>'.html_safe
+      '<button class="read-link">Mark as Unread</button>'.html_safe
     else
-      '<td><button class="unread-link">Mark as Read</button></td>'.html_safe
+      '<button class="unread-link">Mark as Read</button>'.html_safe
     end
   end
 end
---
+++ spec/features/user_can_edit_a_link_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.feature "User can edit a link" do
+  context 'they click edit on their homepage' do
+    scenario "they fill out a form and the changes appear on their homepage" do
+
+      user = User.create(email_address: 'david@example.com', password: 'password')
+      link = user.links.create(url: 'https://www.turing.io', title: 'Turing')
+
+      login(user)
+
+      click_on 'My Links'
+
+      within('#links-table') do
+        expect(page).to have_content(link.url)
+        expect(page).to have_content(link.title)
+        expect(page).to have_content(link.read)
+      end
+
+      click_button 'Edit'
+
+      expect(current_path).to eq edit_link_path(link)
+
+      within('#edit-link-form') do
+        fill_in 'link[title]', with: 'Google'
+        fill_in 'link[url]', with: 'http://www.google.com'
+        click_on 'Save'
+      end
+
+      expect(current_path).to eq links_path
+
+      within('#links-table') do
+        expect(page).to_not have_content(link.url)
+        expect(page).to_not have_content(link.title)
+        expect(page).to have_content(link.read)
+        expect(page).to have_content('Google')
+        expect(page).to have_content('http://www.google.com')
+      end
+    end
+  end
+end
