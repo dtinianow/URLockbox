9a082ff178cc1cf02528c1e736258600a6075221
David Tinianow <david.tinianow@gmail.com>
2016-10-31 16:38:52 -0600

    Begin adding dynamic search function

--- app/assets/javascripts/link_status.js
+++ app/assets/javascripts/link_status.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   onClickChangeReadStatus();
+  searchLinks();
 });
 
 function onClickChangeReadStatus() {
@@ -25,9 +26,29 @@ function changeReadStatus(newStatus, link_id, user_id, $linkStatus, $linkButton)
   if ($linkButton.text() == 'Mark as Read') {
     $linkButton.text('Mark as Unread');
   } else {
-      $linkButton.text('Mark as Read');
-    };
-    // console.log($linkButton.text())
+    $linkButton.text('Mark as Read');
   };
+};
 
-  function handleError(error) { console.log(error) };
+function handleError(error) { console.log(error) };
+
+
+function searchLinks() {
+  $('#linkFilter').on('keyup', function(){
+    var $searchQuery = $('#linkFilter').val()
+    checkMatches($searchQuery)
+  })
+}
+
+function checkMatches(searchQuery) {
+  var $links = $('#links-table').find('.link')
+  $.each($links, function(index, link){
+    var title = $(link).find('.link-title').text()
+    var url = $(link).find('.link-url').text()
+    if (title.indexOf(searchQuery) >= 0 || url.indexOf(searchQuery) >= 0) {
+      $(link).show();
+    } else {
+      $(link).hide();
+    }
+  })
+}
