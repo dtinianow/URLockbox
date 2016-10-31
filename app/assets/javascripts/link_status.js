f004636c2f1dc6a17dea79b9764bbcdef15bf5cb
David Tinianow <david.tinianow@gmail.com>
2016-10-31 15:55:57 -0600

    Able to change read status in one direction with ajax

--- app/assets/javascripts/link_status.js
+++ app/assets/javascripts/link_status.js
@@ -5,19 +5,27 @@ $(document).ready(function() {
 function onClickChangeReadStatus() {
   $('#links-table').on('click', '.read-link', function() {
     var $link = $(this).closest('tr');
+    var link_id = $link.data('link-id');
+    var user_id = $link.data('user-id');
     var $linkStatus = $link.find('.link-read-status')
     var status = $linkStatus.text();
     var newStatus = !status;
-    console.log(newStatus)
-    // changeReadStatus();
+    changeReadStatus(newStatus, link_id, user_id, $linkStatus);
   })
 };
 
-// function changeReadStatus(newStatus) {
-//   $.ajax({
-//
-//   })
-// }
+function changeReadStatus(newStatus, link_id, user_id, $linkStatus) {
+  $.ajax({
+    url: "/api/v1/users/" + user_id + "/links/" + link_id,
+    type: 'put',
+    data: { link: { read: newStatus } }
+  }).fail(handleError)
+  $linkStatus.text(newStatus);
+}
+
+
+function handleError(error) { console.log(error) };
+
 
 // function editIdeaQuality(newQuality, $idea, $quality){
 //   $.ajax({
