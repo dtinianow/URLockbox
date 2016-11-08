63f8d0b4561939c15611ef3e687688e0e28c2e05
David Tinianow <david.tinianow@gmail.com>
2016-11-08 10:11:49 -0700

    Clean up links controller syntax and make links clickable with styling updates

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,30 +1,25 @@
 class LinksController < ApplicationController
   def index
-    @links = Link.where(user: current_user)
+    @links = current_user.links
   end
 
   def create
-    link = Link.new(
-        user_id: current_user.id,
-        title: link_params['title'],
-        url: link_params['url']
-      )
+    link = current_user.links.new(link_params)
     if link.save
       flash[:success] = "Successfully created link!"
       redirect_to links_path
     else
-      flash.now[:error] = 'Invalid link. Please make sure you have a valid URL and enter all parameters.'
-      @links = Link.where(user: current_user)
-      render :index
+      flash[:error] = 'Invalid link. Please make sure you have a valid URL and enter all parameters.'
+      redirect_to links_path
     end
   end
 
   def edit
-    @link = Link.find_by(user: current_user, id: params[:id])
+    @link = current_user.links.find(params[:id])
   end
 
   def update
-    @link = Link.find_by(user: current_user, id: params[:id])
+    @link = current_user.links.find(params[:id])
     if @link.update(link_params)
       flash[:success] = 'Successfully updated link.'
       redirect_to links_path
